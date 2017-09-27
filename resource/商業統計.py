d10d6258a43787b630992a05f40f92b7a1ad1f9a
Your Name <gim.kobayashi@gmail.com>
2017-09-28 02:11:53 +0900

    update

--- resource/商業統計.py
+++ resource/商業統計.py
@@ -10,12 +10,21 @@ for vals in f:
   vals = re.split(r'\s{1,}', vals.strip())
   obj = dict( zip(keys, vals) )
   print( obj ) 
-  for k in obj.keys():
+  for k in list(obj.keys()):
     if re.search('\d{4}',k):
       obj[k] = obj[k].replace(',', '')
       try:
         obj[k] = float( obj[k] )
       except:
         obj[k] = None
+    if k == '市区町村名':
+      obj['contry'] = obj[k]
+      del obj[k]
+    if k == '市区町村コード':
+      obj['city'] = obj[k]
+      del obj[k]
+    if '年' in k:
+      obj[ k.replace('年', '') ] = obj[k]
+      del obj[k]
   print('a', obj )
   w.write( json.dumps(obj, ensure_ascii=False) + '\n' )
